d6b56421dc508d5fe9d820e4d0474d4e271785a9
Cooper <cdickson@unb.ca>
2024-10-24 14:50:51 -0300

    uploading simulation script

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# mock-controller
+# mock-controller - Simulator for Controller via MQTT
+
+This Python script simulates a controller by generating mock data and publishing it to an MQTT broker. The data represents values that any connected DER would produce like current output and time and is transmitted at 1 second intervals.
+
+## Features
+
+- Publishes data to a HIVEMQ MQTT broker at 1 second intervals
+- Publishes DER data between 1 and 5 times to simulate multiple connected DERs
+
+## Data Format 
+
+```json
+{
+    "derId": "12",
+    "type": "solar",
+    "isOnline": true,
+    "Timestamp": "2024-10-23T11:51:39.233518",
+    "currentOutput": 7,
+    "units": "kW",
+    "projectId": "project1234",
+    "utilityId": "utility1234",
+    "isStandalone": false,
+    "currentSoc": 0
+}
+
+
---
+++ src/simulate_data.py
@@ -0,0 +1,83 @@
+from random import randrange
+import datetime
+import os
+import time
+import paho.mqtt.client as mqtt
+import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = os.getenv('URL')
+PORT = int(os.getenv('PORT'))
+USERNAME = "gridstream"
+PASSWORD = os.getenv('PASSWORD')
+
+
+def generate_data():
+    data = []
+    for i in range(randrange(1, 5)):
+        der = {
+            "derId": "12",
+            "type": "solar",
+            "isOnline": True,
+            "Timestamp": datetime.datetime.now().isoformat(),
+            "currentOutput": 7,
+            "units": "kW",
+            "projectId": "adsf1234dfgr1234",
+            "utilityId": "utility1234",
+            "isStandalone": False,
+            "connectionStartAt": "",
+            "currentSoc": 0
+        }
+        data.append(der)
+    return data
+
+
+def create_client(username, password):
+    client = mqtt.Client(callback_api_version=mqtt.CallbackAPIVersion.VERSION2, client_id="gridstream")
+    client.tls_set(tls_version=mqtt.ssl.PROTOCOL_TLS)
+    client.user_data_set([])
+    client.username_pw_set(username, password)
+    return client
+
+
+def connect(client):
+    response = client.connect(URL, PORT)
+    if response == 0:
+        print("Connected to broker successfully")
+    else:
+        print(f"Failed to connect. Error code: {response}")
+
+
+def publish(client, data):
+    project_id = data[0]['projectId']
+    payload = json.dumps(data, indent=4)
+    response = client.publish(f"projects/{project_id}", payload=payload, qos=0)
+    if response.rc == 0:
+        print(f"Published to Topic: projects/{project_id}\n{payload}")
+    else:
+        print(f"Failed to publish message. Error: {response.rc}")
+
+
+def main():
+    client = create_client(USERNAME, PASSWORD)
+    connect(client)
+
+    client.loop_start()
+    try:
+        while True:
+            data = generate_data()
+            publish(client, data)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print("\nDisconnecting from broker...")
+        client.loop_stop()
+        client.disconnect()
+        print("Disconnected successfully.")
+
+
+if __name__ == "__main__":
+    main()
